fd485d6371cfdfbd441f273ca283c2aa071313ce
johan Bautista <jsbparra@gmail.com>
2020-01-24 23:21:09 +0100

    page-projects part. 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+import { BlogComponent } from './pages/blog/blog.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    PortfolioComponent,
+    BlogComponent
   ],
   imports: [
     BrowserModule,
